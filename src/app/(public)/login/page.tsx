dff3854a7461f97843fbc11a7eb0f859c8dbb2c1
mateus <mateushbsb@gmail.com>
2025-04-14 17:06:52 -0300

    feat: login

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -11,11 +11,15 @@ export default function Home() {
         className="transition-transform duration-500 transform scale-100 hover:scale-110"
       />
       <div className="w-80 h-80 flex flex-col justify-center bg-gray-300 p-8 gap-4 rounded-sm">
-        <label className="font-bold text-black">E-mail</label>
-        <input className="bg-stone-50 rounded-sm h-10" type="email" />
-        <label className="font-bold text-black">Senha</label>
-        <input className="bg-stone-50 rounded-sm h-10" type="password" />
-        <button className="bg-emerald-900 text-stone-50 rounded-sm h-10 " type="button">Entrar</button>
+        <div className="flex flex-col">
+          <label className="font-bold text-black">E-mail</label>
+          <input className="bg-stone-50 rounded-sm h-10" type="email" />
+        </div>
+        <div className="flex flex-col">
+          <label className="font-bold text-black">Senha</label>
+          <input className="bg-stone-50 rounded-sm h-10" type="password" />
+        </div>
+        <button className="bg-emerald-900 text-stone-50 rounded-sm h-10 font-bold" type="button">Entrar</button>
       </div>
     </div>
   );
