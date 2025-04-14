6be65c1ab807d285bd8b8ae6d3afa2e4f523213b
mateus <mateushbsb@gmail.com>
2025-04-14 15:28:52 -0300

    feat: login view

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="w-screen h-screen flex flex-col items-center gap-4 justify-center bg-stone-50">
+      <Image 
+        src="/images/logo-itafos.png" 
+        alt="Logo" 
+        width={150} 
+        height={150} 
+        className="transition-transform duration-500 transform scale-100 hover:scale-110"
+      />
+      <div className="w-80 h-80 flex flex-col justify-center bg-gray-300 p-8 gap-4 rounded-sm">
+        <label className="font-bold text-black">E-mail</label>
+        <input className="bg-stone-50 rounded-sm h-10" type="email" />
+        <label className="font-bold text-black">Senha</label>
+        <input className="bg-stone-50 rounded-sm h-10" type="password" />
+        <button className="bg-emerald-900 text-stone-50 rounded-sm h-10 " type="button">Entrar</button>
+      </div>
     </div>
   );
 }
+
