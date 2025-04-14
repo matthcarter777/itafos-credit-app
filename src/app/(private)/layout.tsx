78530e3ef268902abd661695c9037238b73c5c4b
mateus <mateushbsb@gmail.com>
2025-04-14 11:32:33 -0300

    feat: pages

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ITAFOS CREDITO",
+  description: "Cadastro de analise de credito ITAFOS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <h1>Privado</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
