a04e48713acb46273279da6ffceee1d84993f000
mateus <mateushbsb@gmail.com>
2025-05-07 08:25:24 -0300

    feat: add delete button on fazenda component

--- src/app/components/FazendaContainer/index.tsx
+++ src/app/components/FazendaContainer/index.tsx
@@ -25,6 +25,13 @@ const FazendasContainer: React.FC<AtividadeTableProps> = ({data}) => {
       {data?.map((fazenda, index) => {
         return (
           <div key={fazenda.id} className='p-2 bg-white rounded-sm mb-4 relative'>
+            <button
+              onClick={() => removerParecer(fazenda.id)}
+              className="absolute top-2 right-2 text-gray-400 hover:text-red-500"
+              aria-label="Remover parecer"
+            >
+              <X size={18} />
+            </button>
             <div className='grid grid-cols-4 gap-4 mt-4 mb-6'>
               <Textbox title='Nome' data={fazenda.nome} isHidde={!!!fazenda.nome}/>
               <Textbox title='Cidade' data={fazenda.cidade} isHidde={!!!fazenda.cidade}/>
