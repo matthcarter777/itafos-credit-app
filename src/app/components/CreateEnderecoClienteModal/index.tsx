c9d461851f1025fb279a31255966a4789cab1300
mateus <mateushbsb@gmail.com>
2025-05-07 16:30:16 -0300

    feat: update interationa on create endereço

--- src/app/components/CreateEnderecoClienteModal/index.tsx
+++ src/app/components/CreateEnderecoClienteModal/index.tsx
@@ -5,7 +5,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Input from '../Input/Input';
 import { useState } from 'react'
-import { Toast } from '../Toast/Toast';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getEstados } from '@/app/services/hooks/getEstados';
 import Select from '../Select/Select';
@@ -13,7 +12,7 @@ import { Estado } from '@/app/types/Estado';
 import { Municipio } from '@/app/types/Municipio';
 import { getMunicipios } from '@/app/services/hooks/getMunicipio';
 import { createEnderecoCliente } from '@/app/services/create/CreateEnderecoCliente';
-
+import toast from 'react-hot-toast';
 
 
 type CreateEnderecoClienteModalProps = {
@@ -24,17 +23,17 @@ type CreateEnderecoClienteModalProps = {
 const createEnderecoClienteSchema = z.object({
   logradouro: z.string().nonempty('O logradouro é obrigatório.'),
   numero: z.string().nullable().optional(),
-  uf: z.string().nonempty('A UF (estado) é obrigatória.'),
+  uf: z.string().nonempty('O estado (UF) é obrigatório.'),
   cidade: z.string().nonempty('A cidade é obrigatória.'),
   bairro: z.string().nonempty('O bairro é obrigatório.'),
   cep: z.string().nonempty('O CEP é obrigatório.'),
   complemento: z.string().nonempty('O complemento é obrigatório.'),
 });
 
+
 type CreateEnderecoClienteSchema = z.infer<typeof createEnderecoClienteSchema>;
 export default function CreateEnderecoClienteModal({ title, clienteId }: CreateEnderecoClienteModalProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [showToast, setShowToast] = useState(false);
 
   const estados = useQuery<Estado[]>({ queryKey: ['estados'], queryFn: getEstados });
 
@@ -44,11 +43,11 @@ export default function CreateEnderecoClienteModal({ title, clienteId }: CreateE
     mutationFn: createEnderecoCliente,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['clientes'] });
-      setShowToast(true);
       setIsOpen(false);
+      toast.success('Endereço cadastrado com sucesso!');
     },
     onError: (error) => {
-      console.error('Erro ao criar RTV:', error);
+      toast.error('Erro ao cadastrado endereço.');
     },
   });
 
@@ -111,13 +110,14 @@ export default function CreateEnderecoClienteModal({ title, clienteId }: CreateE
             className="bg-gray-300 p-6 rounded shadow-lg w-full max-w-md flex flex-col gap-3" 
             onSubmit={handleSubmit(handleFormSubmit)} >
             <h2 className="text-xl font-bold mb-4">{title}</h2>
-              <Select<CreateEnderecoClienteSchema>
-              label="UF"
+            <Select<CreateEnderecoClienteSchema>
+              label="Estado (UF)"
               name="uf"
               register={register}
               errors={errors}
               options={estados.data || []}
             />
+
             <Select<CreateEnderecoClienteSchema>
               label="Cidade"
               name="cidade"
@@ -125,41 +125,46 @@ export default function CreateEnderecoClienteModal({ title, clienteId }: CreateE
               errors={errors}
               options={municipios.data || []}
             />
+
             <Input<CreateEnderecoClienteSchema>
               label="Endereço"
               name="logradouro"
               register={register}
               errors={errors}
-              placeholder="Digite o nome do produto"
+              placeholder="Ex: Rua das Flores, Avenida Brasil"
             />
+
             <Input<CreateEnderecoClienteSchema>
               label="CEP"
               name="cep"
               register={register}
               errors={errors}
-              placeholder="Digite o nome do produto"
+              placeholder="Digite o CEP (somente números)"
             />
+
             <Input<CreateEnderecoClienteSchema>
               label="Bairro"
               name="bairro"
               register={register}
               errors={errors}
-              placeholder="Digite o nome do produto"
+              placeholder="Digite o nome do bairro"
             />
+
             <Input<CreateEnderecoClienteSchema>
-              label="Numero"
-              type='number'
+              label="Número"
+              type="number"
               name="numero"
               register={register}
               errors={errors}
-              placeholder="Digite o nome do produto"
+              placeholder="Número da residência (opcional)"
             />
+
             <Input<CreateEnderecoClienteSchema>
               label="Complemento"
               name="complemento"
               register={register}
               errors={errors}
-              placeholder="Digite o nome do produto"
+              placeholder="Ex: Apto 101, Fundos, Bloco B"
             />
             <button
               className="px-4 py-2 bg-emerald-700  text-white rounded hover:bg-emerald-800 transition"
@@ -174,13 +179,6 @@ export default function CreateEnderecoClienteModal({ title, clienteId }: CreateE
               Fechar
             </button>
           </form>
-
-          {showToast && (
-            <Toast
-              message="Usuario salvos com sucesso!"
-              onClose={() => setShowToast(false)}
-            />
-          )}
         </div>
       )}
     </div>
